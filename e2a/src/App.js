509516bb5856057e3627daafe3f44d7e3eb7161c
juliatag <julieta.jabbot@gmail.com>
2022-11-23 14:12:26 -0500

    first commit

---
+++ e2a/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <p>Project E2a : from scratch using npm init </p>
+      <p>by : Julieta G. </p>
+    </div>
+  );
+}
+export default App;
