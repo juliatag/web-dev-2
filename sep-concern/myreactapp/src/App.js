eb471e0b41e5381b6680d84abaf04e931c083af4
juliatag <julieta.jabbot@gmail.com>
2022-11-25 11:03:20 -0500

    added outlet mainLaout

--- sep-concern/myreactapp/src/App.js
+++ sep-concern/myreactapp/src/App.js
@@ -3,14 +3,25 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes> */}
+    </>
   );
 }
 export default App;
