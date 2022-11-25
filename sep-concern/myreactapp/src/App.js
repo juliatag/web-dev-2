4a24e22e8a97ab786105b819be33137915b659bb
juliatag <julieta.jabbot@gmail.com>
2022-11-25 09:47:34 -0500

    added routing and pages

--- sep-concern/myreactapp/src/App.js
+++ sep-concern/myreactapp/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
-import Main from "./components/Main";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <Main />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 }
 export default App;
